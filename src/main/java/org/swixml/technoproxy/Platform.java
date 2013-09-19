a84d0436dd3caf60fb34d44e18392d0724ada6d9
Lionel Benychou <lionel.benychou@gmail.com>
2013-09-19 16:40:20 +0200

    end

--- src/main/java/org/swixml/technoproxy/Platform.java
+++ src/main/java/org/swixml/technoproxy/Platform.java
@@ -2,5 +2,6 @@ package org.swixml.technoproxy;
 
 public class Platform {
 
-    public static String NAME = System.getProperty ("swixml.platform");
+    public static String NAME = (System.getProperty ("swixml.platform") == null ?
+            "swing" : System.getProperty ("swixml.platform"));
 }
